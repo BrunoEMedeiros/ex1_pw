9ac790c1158f0ff2d8ac969d14088e31be8f56e1
unknown <bruno.emedeiros99@gmail.com>
2022-05-16 09:19:09 -0300

    exercicio semanal 1

---
+++ src/views/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Imagem from '../components/Imagem';
+
+import styles from './App.css'
+
+const Tela = () =>
+{
+    return <>
+        <div className='cssContainer'>
+            <Imagem />
+            <p>bfvgeuawyfgeawuoewUFGEOU</p>
+            <p>bfvgeuawyfgeawuoewUFGEOU</p>
+
+            <p>bfvgeuawyfgeawuoewUFGEOU</p>
+
+        </div>
+    </>
+}
+
+export default Tela;
